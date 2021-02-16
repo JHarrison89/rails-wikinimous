f1068cd09d6ba0f57cf1fd728d7f0cac407e0a9e
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-02-16 16:35:29 +0000

    adding all CRUD actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,22 +4,41 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def create
+  def new
+    @article = Article.new
   end
 
-  def new
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to article_path(@article)
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to article_path(@article) # redirects to show page
   end
 
   def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
